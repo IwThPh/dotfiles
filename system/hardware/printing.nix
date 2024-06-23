12b8c96da34f03d0b09809103d596eda793b41c4
Iwan Phillips <iwanthomphil@gmail.com>
2024-06-23 19:14:13 +0100

    --wip-- [skip ci]

--- system/hardware/printing.nix
+++ system/hardware/printing.nix
@@ -1,2 +1,11 @@
+{ pkgs, ... }:
 
+{
+  # Enable printing
+  services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns4 = true;
+  services.avahi.openFirewall = true;
+  environment.systemPackages = [ pkgs.cups-filters ];
+}
 
