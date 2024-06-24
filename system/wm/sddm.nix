0aee1e5adaa2186a0dead026df7fe4e88e2f0686
Iwan Phillips <iwan@iwanphillips.dev>
2024-06-24 23:39:14 +0100

    --wip-- [skip ci]

--- system/wm/sddm.nix
+++ system/wm/sddm.nix
@@ -2,10 +2,10 @@
 
 pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
-  src = pkgs.fetchGit {
+  src = pkgs.fetchgit {
     url = "https://framagit.org/MarianArlt/sddm-sugar-candy.git";
     rev = "2b72ef6c6f720fe0ffde5ea5c7c48152e02f6c4f";
-    hash = "1db4p2d0k5a6avr7dg9h1r7y9mx711ci5dgwmljqjb8pq5b0a22y";
+    sha256 = "1db4p2d0k5a6avr7dg9h1r7y9mx711ci5dgwmljqjb8pq5b0a22y";
   };
   installPhase = ''
     mkdir -p $out
