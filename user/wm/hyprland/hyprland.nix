de3aa59af27192c39712bf648c6ac8f594f8a10f
Iwan Phillips <iwan@iwanphillips.dev>
2024-06-25 21:56:02 +0100

    --wip-- [skip ci]

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -1,6 +1,6 @@
-{ inputs, config, lib, pkgs, userSettings, systemSettings, pkgs-nwg-dock-hyprland, ... }:
+{ inputs, config, lib, pkgs, userSettings, systemSettings, ... }:
 let
-  pkgs-hyprland = pkgs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  pkgs-hyprland = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in
 {
   imports = [
@@ -265,13 +265,121 @@ in
         killall wlsunset &> /dev/null;
       fi
     '')
-    ]);
+    ] ++ (
+      with pkgs-hyprland; [ hyprlock ]
+    ));
 
     home.file.".config/libinput-gestures.conf".text = ''
       gesture swipe up 3	hyprctl dispatch hycov:toggleoverview
       '';
       # gesture swipe right 3 workspace-1	
       # gesture swipe left 3 workspace+1	
+
+    home.file.".config/hypr/hypridle.conf".text = ''
+      general {
+          lock_cmd = pidof hyprlock || hyprlock       # avoid starting multiple hyprlock instances.
+          before_sleep_cmd = loginctl lock-session    # lock before suspend.
+          after_sleep_cmd = hyprctl dispatch dpms on  # to avoid having to press a key twice to turn on the display.
+          ignore_dbus_inhibit = false
+      }
+
+      listener {
+          timeout = 300 # in seconds
+          on-timeout = loginctl lock-session
+      }
+
+      listener {
+          timeout = 1800 # in seconds
+          on-timeout = systemctl suspend
+      }
+    '';
+
+    home.file.".config/hypr/hyprlock.conf".text = let 
+        colors = config.lib.stylix.colors;
+    in ''
+      background {
+        monitor =
+        path = screenshot
+
+        # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
+        blur_passes = 4
+        blur_size = 5
+        noise = 0.0117
+        contrast = 0.8916
+        brightness = 0.8172
+        vibrancy = 0.1696
+        vibrancy_darkness = 0.0
+      }
+
+      image {
+        monitor =
+        path = ''+config.stylix.image+''
+        size = 150 # lesser side if not 1:1 ratio
+        rounding = -1 # negative values mean circle
+        border_size = 0
+        rotate = 0 # degrees, counter-clockwise
+
+        position = 0, 200
+        halign = center
+        valign = center
+      }
+
+      input-field {
+        monitor =
+        size = 250, 50
+        outline_thickness = 3
+        dots_size = 0.33 # Scale of input-field height, 0.2 - 0.8
+        dots_spacing = 0.15 # Scale of dots' absolute size, 0.0 - 1.0
+        dots_center = false
+        dots_rounding = -1 # -1 default circle, -2 follow input-field rounding
+        outer_color = rgb(''+colors.base07-rgb-r+'',''+colors.base07-rgb-g+'', ''+colors.base07-rgb-b+'')
+        inner_color = rgb(''+colors.base00-rgb-r+'',''+colors.base00-rgb-g+'', ''+colors.base00-rgb-b+'')
+        font_color = rgb(''+colors.base07-rgb-r+'',''+colors.base07-rgb-g+'', ''+colors.base07-rgb-b+'')
+        fade_on_empty = true
+        fade_timeout = 1000 # Milliseconds before fade_on_empty is triggered.
+        placeholder_text = <i>Input Password...</i> # Text rendered in the input box when it's empty.
+        hide_input = false
+        rounding = -1 # -1 means complete rounding (circle/oval)
+        check_color = rgb(''+colors.base0A-rgb-r+'',''+colors.base0A-rgb-g+'', ''+colors.base0A-rgb-b+'')
+        fail_color = rgb(''+colors.base08-rgb-r+'',''+colors.base08-rgb-g+'', ''+colors.base08-rgb-b+'')
+        fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i> # can be set to empty
+        fail_transition = 300 # transition time in ms between normal outer_color and fail_color
+        capslock_color = -1
+        numlock_color = -1
+        bothlock_color = -1 # when both locks are active. -1 means don't change outer color (same for above)
+        invert_numlock = false # change color if numlock is off
+        swap_font_color = false # see below
+
+        position = 0, -20
+        halign = center
+        valign = center
+      }
+
+      label {
+        monitor =
+        text = Session Locked
+        color = rgb(''+colors.base07-rgb-r+'',''+colors.base07-rgb-g+'', ''+colors.base07-rgb-b+'')
+        font_size = 24
+        font_family = ''+userSettings.font+''
+        rotate = 0 # degrees, counter-clockwise
+        position = 0, 160
+        halign = center
+        valign = top
+      }
+
+      label {
+        monitor =
+        text = $TIME
+        color = rgb(''+colors.base07-rgb-r+'',''+colors.base07-rgb-g+'', ''+colors.base07-rgb-b+'')
+        font_size = 20
+        font_family = Intel One Mono
+        rotate = 0 # degrees, counter-clockwise
+
+        position = 0, 80
+        halign = center
+        valign = center
+      }
+    '';
 
     home.file.".config/waybar/config".text = ''
         // -*- mode: jsonc -*-
