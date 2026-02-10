4c6ab557697a30af5b72032047086d3318e67a64
Iwan Phillips <iwan@iwanphillips.dev>
2026-02-10 18:20:05 +0000

    feat(zsh): add claude alias and update zellij helper

--- stow/zsh/.config/zsh/aliases.sh
+++ stow/zsh/.config/zsh/aliases.sh
@@ -1,7 +1,13 @@
+alias c='claude'
 alias art='php artisan'
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
 
 # alias -g phpstan='docker run -v $PWD:/app --rm ghcr.io/phpstan/phpstan'
 
-alias z='zellij --config ~/.config/zellij/config.kdl'
-alias zb='zellij attach --create-background'
+z() {
+  if [[ -n "$1" ]]; then
+    zellij attach --create "$1"
+  else
+    zellij
+  fi
+}
