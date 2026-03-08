305e738cc1f1bb0c39175df5454726cd281416c9
Iwan Phillips <iwan@iwanphillips.dev>
2026-03-08 17:48:17 +0000

    fix: copy termux font instead of symlinking

--- nix/modules/android/default.nix
+++ nix/modules/android/default.nix
@@ -13,8 +13,9 @@
   home.homeDirectory = "/data/data/com.termux.nix/files/home";
   home.stateVersion = "24.05";
 
-  home.file.".termux/font.ttf".source =
-    "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf";
+  home.activation.termuxFont = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    install -Dm644 "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf" "$HOME/.termux/font.ttf"
+  '';
 
   home.packages = with pkgs; [
     openssh
