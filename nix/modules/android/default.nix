2e41cf37763bc1e77f515242693668b7d245f404
Iwan Phillips <iwan@iwanphillips.dev>
2026-03-08 17:42:13 +0000

    feat(android): add JetBrains Mono Nerd Font

--- nix/modules/android/default.nix
+++ nix/modules/android/default.nix
@@ -13,8 +13,12 @@
   home.homeDirectory = "/data/data/com.termux.nix/files/home";
   home.stateVersion = "24.05";
 
+  home.file.".termux/font.ttf".source =
+    "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf";
+
   home.packages = with pkgs; [
     openssh
+    nerd-fonts.jetbrains-mono
     neovim
     ripgrep
     fzf
